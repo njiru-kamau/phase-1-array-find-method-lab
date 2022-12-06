28e010e5b810cb27729db08f031153d09d159dfe
njiru-kamau <118248319+njiru-kamau@users.noreply.github.com>
2022-12-06 07:44:56 +0300

    Create index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function superbowlWin() {
+    var superbowlWin = document.createElement('div');
+    superbowlWin.innerHTML = 'Superbowl Win';
+    document.body.appendChild(superbowlWin);
+  }
+  
+  function superbowlWin(teams) {
+    var superbowlWin = document.createElement('div');
+    superbowlWin.innerHTML = 'Superbowl Win';
+    document.body.appendChild(superbowlWin);
+  }
+
+  function superbowlWin(teams) {
+    var superbowlWin = document.createElement('div');
+    superbowlWin.innerHTML = 'Superbowl Win';
+    document.body.appendChild(superbowlWin);
+  }
